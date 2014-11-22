d210c4fd87622c3b644b605773df59b08daf17a3
Joshua Jones <kilauea.jones@gmail.com>
2014-11-21 20:37:43 -0600

    Initial add of project.

---
+++ opm/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  opm
+//
+//  Created by Joshua Jones on 11/21/14.
+//  Copyright (c) 2014 Joshua Jones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    
+    return 0;
+}
